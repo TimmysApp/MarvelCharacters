4eebe1d9aab95ecff7867a513173d1a0a52f37a6
Joe Maghzal <joemaghzal@icloud.com>
2023-09-24 16:03:24 +0300

    Add routes for comics, stories, events, series; update xcdatamodel to support CharacterContent model

--- Marvel Characters/Data/Network/Routes/CharactersRoute.swift
+++ Marvel Characters/Data/Network/Routes/CharactersRoute.swift
@@ -9,22 +9,33 @@ import Foundation
 import NetworkUI
 
 enum CharactersRoute: Route {
-    case characters(parameters: CharactersParameters)
+    case characters(parameters: CharactersParameters), comics(characterID: Int), events(characterID: Int), stories(characterID: Int), series(characterID: Int)
     var route: URLRoute {
         let authenticationParameters = AuthenticationParametersFactory.assemble()
         return URLRoute(from: "characters")
-            .appending(authenticationParameters)
             .appending {
+                switch self {
+                    case .comics(let characterID), .events(let characterID), .stories(let characterID), .series(let characterID):
+                        return String(characterID)
+                    default:
+                        return nil
+                }
+            }.appending {
                 switch self {
                     case .characters(let parameters):
                         return parameters
+                    case .comics:
+                        return "comics"
+                    case .events:
+                        return "events"
+                    case .stories:
+                        return "stories"
+                    case .series:
+                        return "series"
                 }
-            }
+            }.appending(authenticationParameters)
     }
     var method: RequestMethod {
-        switch self {
-            case .characters:
-                return .GET
-        }
+        return .GET
     }
 }
