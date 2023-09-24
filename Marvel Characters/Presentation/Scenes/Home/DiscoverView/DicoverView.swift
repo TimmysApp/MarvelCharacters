3ac8dad0cbe7ea871d11c48047bebcffeb8dc5cf
Joe Maghzal <joemaghzal@icloud.com>
2023-09-24 18:22:52 +0300

    Fix bug where tapping the trailing edge of an image scrolls to the next one in DiscoveryView

--- Marvel Characters/Presentation/Scenes/Home/DiscoverView/DicoverView.swift
+++ Marvel Characters/Presentation/Scenes/Home/DiscoverView/DicoverView.swift
@@ -7,43 +7,51 @@
 
 import SwiftUI
 
-struct Test: Identifiable, Hashable {
-    var id: Int
-}
-
 struct DicoverView: View {
     @ObservedObject var viewModel: HomeViewModel
     @State private var width: CGFloat = 10
     private let spacing: CGFloat = 15
+    private var itemWidth: CGFloat {
+        return (width - (3 * spacing)) / 2.5
+    }
     var body: some View {
-        let glassStyle = Material.ultraThickMaterial.blendMode(.luminosity).opacity(0.8).shadow(.drop(radius: 10))
         VStack(alignment: .leading, spacing: 15) {
-            let selectedCharacter = viewModel.selectedCharacter?.get()
             VStack(alignment: .leading, spacing: 20) {
-                Text(selectedCharacter?.name ?? "")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                    .padding(.top, 100)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                Text(selectedCharacter?.description ?? "")
+                HStack {
+                    Text(viewModel.displayedCharacter?.name ?? "")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                        .padding(.top, 10)
+                    Spacer()
+                    Button(action: {
+                        viewModel.switchStyle(to: .list)
+                    }) {
+                        Image(systemName: "list.dash")
+                            .fontWeight(.semibold)
+                            .frame(height: 40)
+                    }
+                }
+                Text(viewModel.displayedCharacter?.description ?? "")
                     .fontWeight(.medium)
                 Spacer()
-            }.foregroundStyle(glassStyle)
+            }.foregroundStyle(Material.ultraThickMaterial.blendMode(.luminosity).opacity(0.8).shadow(.drop(radius: 10)))
             .padding(.horizontal, 15)
             .background {
-                if viewModel.selectedCharacter == .pagination {
+                if viewModel.displayedCharacterStyle == .pagination {
                     progressView
                         .frame(width: width)
                 }else {
-                    RemotePhotoView(url: selectedCharacter?.thumbnailURL)
+                    RemotePhotoView(url: viewModel.displayedCharacter?.thumbnailURL)
                         .frame(width: width)
                         .clipShape(RoundedRectangle(cornerRadius: 12))
                         .overlay(Color.black.opacity(0.4))
-                        .id(selectedCharacter?.thumbnailURL)
+                        .id(viewModel.displayedCharacter?.thumbnailURL)
+                        .edgesIgnoringSafeArea(.top)
+                        .overlay(alignment: .bottom) {
+                            LinearGradient(colors: [.clear, .black.opacity(0.6), .black.opacity(0.8)], startPoint: .top, endPoint: .bottom)
+                                .frame(height: 50)
+                        }
                 }
-            }.overlay(alignment: .bottom) {
-                LinearGradient(colors: [.clear, .black.opacity(0.6), .black.opacity(0.8)], startPoint: .top, endPoint: .bottom)
-                    .frame(height: 50)
             }
             Text("Marvel Characters")
                 .foregroundColor(.white)
@@ -51,8 +59,7 @@ struct DicoverView: View {
                 .font(.title3)
                 .fontWeight(.semibold)
             HStack {
-                let itemWidth = (width - (3 * spacing)) / 2.5
-                ScrollCarouselView(viewModel.characters, selection: $viewModel.selectedCharacter, width: itemWidth, spacing: spacing) { type in
+                ScrollCarouselView(viewModel.characters, selection: $viewModel.displayedCharacterStyle, width: itemWidth, spacing: spacing) { type in
                     switch type {
                         case .info(let character):
                             RemotePhotoView(url: character.thumbnailURL)
@@ -70,7 +77,6 @@ struct DicoverView: View {
                 }.frame(height: itemWidth * 1.5)
             }
         }.background(Color.black)
-        .edgesIgnoringSafeArea(.top)
         .background {
             GeometryReader { proxy in
                 Color.clear
@@ -78,16 +84,6 @@ struct DicoverView: View {
                         width = proxy.size.width
                     }
             }
-        }.overlay(alignment: .topTrailing) {
-            Button(action: {
-                viewModel.switchStyle(to: .list)
-            }) {
-                Image(systemName: "list.dash")
-                    .fontWeight(.semibold)
-                    .foregroundStyle(glassStyle)
-                    .frame(height: 40)
-            }.padding(.horizontal, 15)
-            .padding(.top, 30)
         }
     }
     private var progressView: some View {
