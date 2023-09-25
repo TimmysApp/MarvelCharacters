4c93e712c82e504216137c22fb7639d041343a9f
Joe Maghzal <joe.maghzal@montymobile.com>
2023-09-25 13:19:47 +0300

    Add test cases for PhotoLoaderTests and refactor PhotoLoader

--- Marvel Characters/Framework/Network/PhotoLoading/PhotoLoader.swift
+++ Marvel Characters/Framework/Network/PhotoLoading/PhotoLoader.swift
@@ -11,34 +11,50 @@ import DataStruct
 
 actor PhotoLoader: ObservableObject {
 //MARK: - Properties
-    private var inMememoryPhotos: [URL: UIImage?] = [:]
     private let objectContext: NSManagedObjectContext
-    private let session: URLSession
+    private let session: PhotoLoaderClient
     private var tasks: [URL: Task<UIImage?, Error>] = [:]
+    var cacheStore = NSCache<NSURL, UIImage>()
+    var runningTasks: Int {
+        return tasks.count
+    }
+    var lastSavedItem: URL?
 //MARK: - Initializer
     init(session: PhotoLoaderClient, objectContext: NSManagedObjectContext, cahingLimit: Int = 100) {
         self.session = session
         self.objectContext = objectContext
         self.cacheStore.countLimit = cahingLimit
     }
-//MARK: - Functions
+//MARK: - Public Functions
     func loadImage(for url: URL) async -> UIImage? {
-        if let memoryImage = inMememoryPhotos[url] {
+        if let memoryImage = retreiveFromMemory(for: url) {
             return memoryImage
         }
         return await fetchImage(for: url)
     }
+    func removeFromMemory(url: URL) {
+        cacheStore.removeObject(forKey: url as NSURL)
+    }
+    func storeInMemory(_ image: UIImage?, for url: URL) {
+        guard let image else {return}
+        cacheStore.setObject(image, forKey: url as NSURL)
+    }
+    func retreiveFromMemory(for url: URL) -> UIImage? {
+        return cacheStore.object(forKey: url as NSURL)
+    }
+//MARK: - Private Functions
     private func fetchImage(for url: URL) async -> UIImage? {
-        if let cachedImage = checkCache(for: url) {
+        if let cachedImage = await checkCache(for: url) {
             storeInMemory(cachedImage, for: url)
             return cachedImage
         }
         guard let task = tasks[url] else {
             let task = Task {
-                let data = try await session.data(from: url).0
-                cache(data: data, for: url)
+                let data = try await session.load(for: url)
+                await cache(data: data, for: url)
                 let image = UIImage(data: data)
                 storeInMemory(image, for: url)
+                tasks.removeValue(forKey: url)
                 return image
             }
             tasks[url] = task
@@ -46,20 +62,30 @@ actor PhotoLoader: ObservableObject {
         }
         return try? await task.value
     }
-    private func cache(data: Data?, for url: URL) {
-        let photoCache = PhotoCache(url: url.absoluteString, data: data)
-        photoCache.save(objectContext)
-    }
-    private func checkCache(for url: URL) -> UIImage? {
-        guard let data = try? PhotoCache.fetch(with: NSPredicate(format: "url == %@", url.absoluteString), objectContext: objectContext).first?.data else {
-            return nil
+    private func cache(data: Data?, for url: URL) async {
+        await withCheckedContinuation { continuation in
+            objectContext.performAndWait {
+                let photoCache = PhotoCache(url: url.absoluteString, data: data)
+                lastSavedItem = url
+                photoCache.save(objectContext)
+                continuation.resume()
+            }
         }
-        return UIImage(data: data)
     }
-    private func removeFromMemory(url: URL) {
-        inMememoryPhotos.removeValue(forKey: url)
-    }
-    private func storeInMemory(_ image: UIImage?, for url: URL) {
-        inMememoryPhotos[url] = image
+    private func checkCache(for url: URL) async -> UIImage? {
+        await withCheckedContinuation { continuation in
+            if  let item = try? PhotoCache.fetch(with: NSPredicate(format: "url == %@", url.absoluteString), objectContext: objectContext).first {
+                if let data = item.data {
+                    continuation.resume(returning: UIImage(data: data))
+                }else {
+                    objectContext.performAndWait {
+                        item.delete(objectContext)
+                        continuation.resume(returning: nil)
+                    }
+                }
+            }else {
+                continuation.resume(returning: nil)
+            }
+        }
     }
 }
---
+++ Marvel CharactersTests/Frameworks/PhotoLoaderTests.swift
@@ -0,0 +1,108 @@
+//
+//  PhotoLoaderTests.swift
+//  Marvel CharactersTests
+//
+//  Created by Joe Maghzal on 25/09/2023.
+//
+
+import XCTest
+import DataStruct
+import CoreData
+@testable import Marvel_Characters
+
+final class PhotoLoaderTests: XCTestCase {
+//MARK: - Properties
+    private let stubURL = URL(string: "google.com")!
+    private var objectContext: NSManagedObjectContext!
+    private let expectedData = UIImage(named: "test")!.pngData()
+//MARK: - Lifecycle
+    override func setUp() async throws {
+        try await super.setUp()
+        objectContext = PersistenceControllerStub().container.newBackgroundContext()
+    }
+    override func tearDown() async throws {
+        try await super.tearDown()
+        objectContext = nil
+    }
+//MARK: - Functions
+    private func loader(expectedData: Data?, delay: UInt64? = nil) -> PhotoLoader {
+        return PhotoLoader(session: PhotoLoaderClientStub(expectedData: expectedData!, delay: delay), objectContext: objectContext)
+    }
+    private func saveStubCache(data: Data?) async {
+        await withCheckedContinuation { continuation in
+            objectContext.performAndWait {
+                let photoCache = PhotoCache(url: stubURL.absoluteString, data: data)
+                photoCache.save(objectContext)
+                continuation.resume()
+            }
+        }
+    }
+//MARK: - Tests
+    func testLoaderCachesDataWhenNotCached() async {
+        let loader = loader(expectedData: expectedData)
+        let loadedImage = await loader.loadImage(for: stubURL)
+        let cachedData = try? PhotoCache.fetch(with: NSPredicate(format: "url == %@", stubURL.absoluteString), objectContext: objectContext).first?.data
+        let lastCachedItem = await loader.lastSavedItem
+        XCTAssertEqual(lastCachedItem, stubURL)
+        XCTAssertEqual(cachedData, expectedData)
+    }
+    func testLoaderRemovesItemWhenDataIsEmpty() async {
+        await saveStubCache(data: expectedData)
+        let loader = loader(expectedData: expectedData)
+        let loadedImage = await loader.loadImage(for: stubURL)
+        let count = try? PhotoCache.fetch(with: NSPredicate(format: "url == %@", stubURL.absoluteString), objectContext: objectContext).count
+        XCTAssertEqual(count, 1)
+    }
+    func testLoaderCachesDataWhenItemSavedWithoutData() async {
+        await saveStubCache(data: expectedData)
+        let loader = loader(expectedData: expectedData)
+        let loadedImage = await loader.loadImage(for: stubURL)
+        let lastCachedItem = await loader.lastSavedItem
+        XCTAssertEqual(lastCachedItem, nil)
+    }
+    func testLoaderDoesNotCacheDataWhenCachedDataFound() async {
+        await saveStubCache(data: expectedData)
+        let loader = loader(expectedData: expectedData)
+        let loadedImage = await loader.loadImage(for: stubURL)
+        let lastCachedItem = await loader.lastSavedItem
+        XCTAssertEqual(lastCachedItem, nil)
+    }
+    func testLoaderStoresTaskForURL() async {
+        let loader = loader(expectedData: expectedData, delay: 100_000_000_000)
+        Task {
+            _ = await loader.loadImage(for: stubURL)
+        }
+        try? await Task.sleep(nanoseconds: 1_000_000)
+        let runningTasks = await loader.runningTasks
+        XCTAssertEqual(runningTasks, 1)
+    }
+    func testLoaderRemovesTaskWhenDone() async {
+        let loader = loader(expectedData: expectedData)
+        Task {
+            _ = await loader.loadImage(for: stubURL)
+        }
+        try? await Task.sleep(nanoseconds: 1_000_000)
+        let runningTasks = await loader.runningTasks
+        XCTAssertEqual(runningTasks, 0)
+    }
+    func testLoaderDoesNotCreateMultipleTasksForSameURL() async {
+        let expectedCalls = Int.random(in: 1..<10)
+        let loader = loader(expectedData: expectedData, delay: 100_000_000_000)
+        Task {
+            for _ in 0..<expectedCalls {
+                _ = await loader.loadImage(for: stubURL)
+                _ = await loader.loadImage(for: stubURL)
+            }
+        }
+        try? await Task.sleep(nanoseconds: 1_000_000)
+        let runningTasks = await loader.runningTasks
+        XCTAssertEqual(runningTasks, 1)
+        XCTAssertNotEqual(runningTasks, expectedCalls)
+    }
+    func testLoaderSavesImageInNSCache() async {
+        let loader = loader(expectedData: expectedData)
+        let loadedImage = await loader.loadImage(for: stubURL)
+        let cachedData = await loader.retreiveFromMemory(for: stubURL)
+        XCTAssertEqual(cachedData, loadedImage)
+    }
+}
